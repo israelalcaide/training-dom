06bf1ae87c1cc514edc0db130608b760a5fe7d72
israelacaide <israelalcaide.aila@gmail.com>
2023-04-04 23:57:53 +0200

    semaforo

---
+++ js/semaforo.js
@@ -0,0 +1,42 @@
+let box1 = document.querySelector("section").firstElementChild;
+let box2 = box1.nextElementSibling;
+let box3 = box2.nextElementSibling;
+
+box1.classList.add("dark");
+box2.classList.add("light");
+setTimeout(() => {
+  box1.classList.remove("dark");
+  box2.classList.toggle("light");
+}, 2000);
+setTimeout(() => {
+  box2.classList.toggle("light");
+}, 1000);
+
+if (box2.classList.contains("light")) {
+  box3.classList.add("dark");
+} else {
+  box2.classList.add("light");
+}
+let on = false;
+setInterval(() => {
+  if (
+    !box1.classList.contains("red") &&
+    !box2.classList.contains("yellow") &&
+    !box3.classList.contains("green")
+  ) {
+    box1.classList.add("red");
+    on = true;
+  } else if (box1.classList.contains("red") && on == false) {
+    box1.classList.remove("red");
+    box2.classList.add("yellow");
+    on = true;
+  } else if (box2.classList.contains("yellow") && on == false) {
+    box2.classList.remove("yellow");
+    box3.classList.add("green");
+    on = true;
+  } else if (box3.classList.contains("green") && on == false) {
+    box3.classList.remove("green");
+    box1.classList.add("red");
+  }
+  on = false;
+}, 3000);
