5243006140df25eae759ef3d14ebef5403a719b8
israelacaide <israelalcaide.aila@gmail.com>
2023-04-08 13:33:05 +0200

    antes de dark-theme

--- js/app.js
+++ js/app.js
@@ -1,58 +1,51 @@
-let span = document.querySelector("span");
+// //document.body.innerHTML="<h1>Waw</h1>"
+// let section = document.querySelector("section");
+// console.log(section.innerHTML);
+// console.log(section.outerHTML);
+// //section.outerHTML="<h1>Waw</h1>"
+// document.body.insertAdjacentHTML("afterbegin", "<h1>Waw</h1>");
+// section.insertAdjacentHTML("beforebegin", "<h2>Hello</h2>");
+// // beforeend - afterend
+// section.insertAdjacentHTML("beforeend", "<span>Holidays</span>");
+// let box2 = section.firstElementChild.nextElementSibling
+// box2.insertAdjacentHTML("afterbegin", "2")
+// section.insertAdjacentHTML("afterend", "<footer>codeteam 12</footer>")
 
-let box1 = document.querySelector("section").firstElementChild;
-let box2 = box1.nextElementSibling;
-let box3 = box2.nextElementSibling;
+// // stronger together
+// document.querySelector("h1").textContent="stronger together"
+// console.log(document.querySelector("h1").textContenr);
 
-let user = document.querySelector("#user");
-let city = document.querySelector("#city");
+let header = document.createElement("h1");
+header.append("Hello World!");
+document.body.append(header);
+header.prepend("!")
+header.firstChild.before("!!")
 
-console.log(box1.dataset.boxNumber);
-console.log(user.dataset.name);
-user.dataset.name ="Sarah Connor"
-console.log(user.dataset.name);
-console.log(city.dataset.cityName);
-city.dataset.cityName="Madrid"
-console.log(city.dataset.cityName);
+let paragraph = document.createElement("p")
+paragraph.textContent= "CodeTeam Best"
+header.after(paragraph)
+paragraph.replaceWith(header)
+document.querySelector("section").remove()
+header.style.backgroundColor="darkolivegreen"
+header.style.padding ="2em"
+header.style.color ="white"
+header.style.fontSize ="3.5rem"
+header.style.textAlign="center"
+header.setAttribute("class","main p-1 m-2")
+//console.log( header.getAttribute("class"));
 
-box1.classList.add("dark");
-box2.classList.add("light");
-setTimeout(() => {
-  box1.classList.remove("dark");
-  box2.classList.toggle("light");
-}, 2000);
-setTimeout(() => {
-  box2.classList.toggle("light");
-}, 1000);
+let hero = {
+  heroName: "hulk",
+  color:"green",
+  power:"super strengh",
+};
+console.log(Object.values(hero));
+let propiedades = Object.values(hero);
+propiedades.forEach((prop)=>{
+  console.log(prop);
+})
 
-if (box2.classList.contains("light")) {
-  box3.classList.add("dark");
-} else {
-  box2.classList.add("light");
-}
-const trafficlight = () =>{  
-
-let on = false;
-setInterval(() => {
-  if (
-    !box1.classList.contains("red") &&
-    !box2.classList.contains("yellow") &&
-    !box3.classList.contains("green")
-  ) {
-    box1.classList.add("red");
-    on = true;
-  } else if (box1.classList.contains("red") && on == false) {
-    box1.classList.remove("red");
-    box2.classList.add("yellow");
-    on = true;
-  } else if (box2.classList.contains("yellow") && on == false) {
-    box2.classList.remove("yellow");
-    box3.classList.add("green");
-    on = true;
-  } else if (box3.classList.contains("green") && on == false) {
-    box3.classList.remove("green");
-    box1.classList.add("red");
-  }
-  on = false;
-}, 3000);
-}
+//let propiedades = Object.keys(header.style);
+//propiedades.forEach((prop)=>{
+//  console.log(header.style[prop]);
+//});
